38a6643ea8b77fb664dfc1eabc0a570b5e0f9b24
TheNoice <61051924+TheNoice@users.noreply.github.com>
2020-03-17 23:33:33 +0300

    summary??
    desc??

---
+++ ComputerButtonSearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public class ComputerButtonSearch
+    {
+        public string Name4Search { get; set; }
+        public string Text4Search { get; set; }
+
+        public bool NameContainsString(Button button)
+        {
+            return button.Name.Contains(Name4Search);
+        }
+        public bool TextContainsString(Button button)
+        {
+            if (Text4Search == "")
+            {
+                return button.Text == string.Empty;
+            }
+            else
+            {
+                return button.Text.Contains(Text4Search);
+            }
+        }
+    }
+}
